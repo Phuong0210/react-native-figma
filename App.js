5d901686c04940e1f53b5ed6d793a72ca6c58037
Phuong <phuong.ho23@student.passerellessnumeriques.org>
2022-11-01 09:49:57 +0700

    01/11/2022 Test

--- App.js
+++ App.js
@@ -1,30 +1,4 @@
-// import * as React from 'react';
-// import { Text, View } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// import Home from './components/Home';
-// import Profile from './components/Profile';
-// function Settings() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
-// const Tab = createBottomTabNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator>
-//         <Tab.Screen name="Home" component={Home} />
-//         <Tab.Screen name="Profile" component={Profile} />
-//         <Tab.Screen name="Settings" component={Settings} />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
 import * as React from 'react';
 import {Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
